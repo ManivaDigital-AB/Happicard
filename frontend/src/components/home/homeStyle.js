15d59229ff6e4eabc9d9c8c45bd78f23a87c8169
Ilir Gashi <ilir.gashi@MBO.mndc>
2021-01-20 10:19:18 +0100

    update read more

--- frontend/src/components/home/homeStyle.js
+++ frontend/src/components/home/homeStyle.js
@@ -3,22 +3,22 @@ import { device } from "../../components/common/device";
 export const BodyContainer = styled.div`
 width: 100%;
    a{
-       text-decoration: none;
-       color:#fff;
-       position: absolute;
-        left: 20%;
-        top: 70%;
-        background: white;
-        color: black;
-        font-size: 1em;
-        font-weight: bold;
-        font-family: Helvetica Neue,Helvetica,sans-serif;
-        margin: 1em;
-        padding: 0.25em 1.25em;
-        border: 2px solid #DFB248;
-        border-radius: 28px;
-        width: 140px;
-        outline: none;
+    text-decoration: none;
+    position: absolute;
+    left: 20%;
+    top: 70%;
+    background: white;
+    color: black;
+    font-size: 1em;
+    font-weight: bold;
+    font-family: "Helvetica Neue", Helvetica, sans-serif;
+    margin: 1em;
+    padding: 5px 20px;
+    border: 2px solid rgb(223, 178, 72);
+    border-radius: 28px;
+    width: 140px;
+    outline: none;
+    white-space: nowrap;
    }
 
    a:hover{
